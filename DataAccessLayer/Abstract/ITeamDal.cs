8bd012016ab6fd79ccd3f1bdb436d36f08663012
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-27 23:04:24 +0300

    Ders 83 - Generic Repository

--- DataAccessLayer/Abstract/ITeamDal.cs
+++ DataAccessLayer/Abstract/ITeamDal.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
+    //: IGenericDal<Team> yapısı generic dalın içinde bulunan tüm özelliklerin bu interface içinde kullanılacağını gösterir
+    //yani generic dalda bulunan özellikler bu interface'e de aktarılır
+    //Sondaki <Team> ifadeside generic dalda bulunan özeliiklerin Team entitysi için uygulanacağını gösterir
     public interface ITeamDal : IGenericDal<Team>
     {
     }
