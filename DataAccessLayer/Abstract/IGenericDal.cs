82f38f25f5abb3896b6e52143776e559f669e189
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-27 22:15:39 +0300

    Ders 82 - Generic Interface

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //where T : class: Bu ifade, T tipinin bir referans tipi (class) olması gerektiğini belirtir.
+    //where T : new (): Bu ifade, T tipinin parametresiz bir kurucuya sahip olması gerektiğini belirtir.
+    public interface IGenericDal<T> where T : class, new()
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        T GetById(int id);
+        List<T> GetListAll();
+
+    }
+}
