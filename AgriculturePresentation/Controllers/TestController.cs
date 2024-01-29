097c655ad3ff0039256f901024c19f1b5c26dfe2
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-29 12:41:08 +0300

    Ders 88 - Service Constructor

---
+++ AgriculturePresentation/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IServiceService _serviceService;
+
+        public TestController(IServiceService serviceService)
+        {
+            this._serviceService = serviceService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceService.GetListAll();
+            return View(values);
+        }
+    }
+}
