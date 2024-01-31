e281886c22496881c505e7fdc59ef263affc67d2
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-31 10:51:05 +0300

    Ders 91 - Yeni Servis Ekleme

--- AgriculturePresentation/Controllers/TestController.cs
+++ AgriculturePresentation/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.Controllers
@@ -12,10 +13,25 @@ namespace AgriculturePresentation.Controllers
             this._serviceService = serviceService;
         }
 
+        //Listeleme İşlemi
         public IActionResult Index()
         {
             var values = _serviceService.GetListAll();
             return View(values);
+        }
+
+        //Ekleme İşlemi
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            _serviceService.Insert(service);
+            return RedirectToAction("Index");
         }
     }
 }
