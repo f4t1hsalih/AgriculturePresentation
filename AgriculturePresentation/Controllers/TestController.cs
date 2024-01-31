f595d2892c69bb91cb66dd5b9bf2d42bfd9f2d5f
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-31 11:10:27 +0300

    Service Add View Model

--- AgriculturePresentation/Controllers/TestController.cs
+++ AgriculturePresentation/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using BusinessLayer.Abstract;
+﻿using AgriculturePresentation.Models;
+using BusinessLayer.Abstract;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,14 +25,24 @@ namespace AgriculturePresentation.Controllers
         [HttpGet]
         public IActionResult AddService()
         {
-            return View();
+            return View(new ServiceAddViewModel());
         }
 
         [HttpPost]
-        public IActionResult AddService(Service service)
+        public IActionResult AddService(ServiceAddViewModel model)
         {
-            _serviceService.Insert(service);
-            return RedirectToAction("Index");
+            //Model çalışıyor ise modelin şartları sağlanıyor ise
+            if (ModelState.IsValid)
+            {
+                _serviceService.Insert(new Service()
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Image = model.Image
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
