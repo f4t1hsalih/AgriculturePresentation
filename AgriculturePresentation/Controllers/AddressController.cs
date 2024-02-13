d70aada195d43dddc003017a42fb5ea75c545906
f4t1hsalih <f4t1hsalih@gmail.com>
2024-02-13 23:59:40 +0300

    Ders 113 - Address İşlemleri

--- AgriculturePresentation/Controllers/AddressController.cs
+++ AgriculturePresentation/Controllers/AddressController.cs
@@ -1,10 +1,8 @@
 ﻿using BusinessLayer.Abstract;
 using BusinessLayer.ValidationRules;
 using EntityLayer.Concrete;
-using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 namespace AgriculturePresentation.Controllers
 {
@@ -24,48 +22,13 @@ namespace AgriculturePresentation.Controllers
         }
 
         [HttpGet]
-        public IActionResult AddAddress()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddAddress(Address address)
-        {
-            AddressValidator validationRules = new AddressValidator();
-            ValidationResult result = validationRules.Validate(address);
-
-            if (result.IsValid)
-            {
-                _addressService.Insert(address);
-                return RedirectToAction("Index");
-            }
-
-            else
-            {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
-                }
-            }
-
-            return View();
-        }
-        public IActionResult DeleteAddress(int id)
-        {
-            var value = _addressService.GetById(id);
-            _addressService.Delete(value);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public IActionResult UpdateAddress(int id)
+        public IActionResult EditAddress(int id)
         {
             var value = _addressService.GetById(id);
             return View(value);
         }
         [HttpPost]
-        public IActionResult UpdateAddress(Address address)
+        public IActionResult EditAddress(Address address)
         {
             AddressValidator validationRules = new AddressValidator();
             ValidationResult result = validationRules.Validate(address);
--- BusinessLayer/ValidationRules/AddressValidator.cs
+++ BusinessLayer/ValidationRules/AddressValidator.cs
@@ -7,10 +7,10 @@ namespace BusinessLayer.ValidationRules
     {
         public AddressValidator()
         {
-            //Map Kuralları
-            RuleFor(x => x.MapInfo).NotEmpty().WithMessage("Başlık Kısmı Boş Bırakılamaz!");
-            RuleFor(x => x.MapInfo).MinimumLength(5).WithMessage("Başlık Minimum 5 Karakter Olmalıdır!");
-            RuleFor(x => x.MapInfo).MaximumLength(20).WithMessage("Başlık Maximum 20 Karakter Olmalıdır!");
+            //Harita Bilgisi Kuralları
+            RuleFor(x => x.MapInfo).NotEmpty().WithMessage("Harita Bilgisi Boş Bırakılamaz!");
+            RuleFor(x => x.MapInfo).MinimumLength(5).WithMessage("Harita Bilgisi Minimum 5 Karakter Olmalıdır!");
+            RuleFor(x => x.MapInfo).MaximumLength(20).WithMessage("Harita Bilgisi Maximum 20 Karakter Olmalıdır!");
 
             //Açıklama Kuralları
             RuleFor(x => x.Description1).NotEmpty().WithMessage("Açıklama 1 Boş Bırakılamazz!!");
