d98fcadfe8f0d4ce7e0807a9d94eee29a98eac43
f4t1hsalih <f4t1hsalih@gmail.com>
2024-03-16 14:52:58 +0300

    Ders 132 - Contact Partial

--- AgriculturePresentation/Controllers/DefaultController.cs
+++ AgriculturePresentation/Controllers/DefaultController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IContactService _contactService;
+
+        public DefaultController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(Contact contact)
+        {
+            contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            _contactService.Insert(contact);
+            return RedirectToAction("Index", "Default");
         }
     }
 }
