045f51e57d232583433588c57e7b4c86f508f811
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-04 14:49:54 +0300

    Ders 151 - Dinamik Mesaj Raporları

--- AgriculturePresentation/Controllers/ReportController.cs
+++ AgriculturePresentation/Controllers/ReportController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using OfficeOpenXml;
-using System;
+﻿using AgriculturePresentation.Models;
+using ClosedXML.Excel; // ClosedXML kütüphanesi eklendi.
+using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml; // OfficeOpenXml kütüphanesi eklendi.
 
 namespace AgriculturePresentation.Controllers
 {
@@ -48,10 +50,53 @@ namespace AgriculturePresentation.Controllers
             }
         }
 
-        // MessageReport eylemi, mesaj raporu için bir görünüm döndürür.
-        public IActionResult MessageReport()
+        // ContactList metodu, veritabanından iletişim bilgilerini almak için kullanılır.
+        public List<ContactModel> ContactList()
         {
-            return View();
+            List<ContactModel> contactModels = new List<ContactModel>();
+            using (var context = new AgricultureContext())
+            {
+                contactModels = context.Contacts.Select(x => new ContactModel
+                {
+                    ContactID = x.ContactID,
+                    ContactName = x.Name,
+                    ContactMail = x.Mail,
+                    ContactMassage = x.Message,
+                    ContactDate = x.Date
+                }).ToList();
+            }
+            return contactModels;
+        }
+
+        // ContactReport eylemi, iletişim bilgilerini içeren bir Excel raporu oluşturur ve indirme işlemi gerçekleştirir.
+        public IActionResult ContactReport()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Mesaj Listesi");
+                workSheet.Cell(1, 1).Value = "Mesaj ID";
+                workSheet.Cell(1, 2).Value = "Mesaj Gönderen";
+                workSheet.Cell(1, 3).Value = "Mail Adresi";
+                workSheet.Cell(1, 4).Value = "Mesaj İçeriği";
+                workSheet.Cell(1, 5).Value = "Mesaj Tarihi";
+
+                int contactRowCount = 2;
+                foreach (var item in ContactList())
+                {
+                    workSheet.Cell(contactRowCount, 1).Value = item.ContactID;
+                    workSheet.Cell(contactRowCount, 2).Value = item.ContactName;
+                    workSheet.Cell(contactRowCount, 3).Value = item.ContactMail;
+                    workSheet.Cell(contactRowCount, 4).Value = item.ContactMassage;
+                    workSheet.Cell(contactRowCount, 5).Value = item.ContactDate;
+                    contactRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Mesaj_Rapor.xlsx");
+                }
+            }
         }
 
         // AnnouncementReport eylemi, duyuru raporu için bir görünüm döndürür.
