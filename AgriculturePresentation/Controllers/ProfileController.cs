53f3ee93841da2ae87ca8c6d0672370b8188b9f2
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-07 16:01:22 +0300

    Ders 162 - Profil Bilgilerini Güncelleme

--- AgriculturePresentation/Controllers/ProfileController.cs
+++ AgriculturePresentation/Controllers/ProfileController.cs
@@ -13,6 +13,7 @@ namespace AgriculturePresentation.Controllers
             _userManager = userManager;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
@@ -20,6 +21,25 @@ namespace AgriculturePresentation.Controllers
             userEditViewModel.Mail = values.Email;
             userEditViewModel.Phone = values.PhoneNumber;
             return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (userEditViewModel.Password==userEditViewModel.ConfirmPassword)
+            {
+                values.Email = userEditViewModel.Mail;
+                values.PhoneNumber = userEditViewModel.Phone;
+                values.PasswordHash = _userManager.PasswordHasher.HashPassword(values, userEditViewModel.Password);
+                var result = await _userManager.UpdateAsync(values);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+            
         }
     }
 }
