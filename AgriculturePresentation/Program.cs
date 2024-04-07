c1b5df55f7399ac4999e33848e9e576606d18e82
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-07 13:28:58 +0300

    Ders 153 - Sol Menü

--- AgriculturePresentation/Program.cs
+++ AgriculturePresentation/Program.cs
@@ -49,6 +49,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Service}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
