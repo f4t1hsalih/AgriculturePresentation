a0c0d6c3da78fd40c54aa7fd5c8708ba82785d1a
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-07 15:54:53 +0300

    Ders 161 - Profile Controller

---
+++ AgriculturePresentation/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using AgriculturePresentation.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Mail = values.Email;
+            userEditViewModel.Phone = values.PhoneNumber;
+            return View(userEditViewModel);
+        }
+    }
+}
