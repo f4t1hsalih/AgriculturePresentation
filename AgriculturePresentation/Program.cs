5e880437817ab6e020623ee3ec00c73549bdc818
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-07 14:35:28 +0300

    Ders 157 - Startup Konfügürasyonu

--- AgriculturePresentation/Program.cs
+++ AgriculturePresentation/Program.cs
@@ -5,6 +5,7 @@ using DataAccessLayer.Concrete.EntityFramework;
 using DataAccessLayer.Contexts;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 //FS
 
@@ -31,6 +32,7 @@ builder.Services.AddScoped<IAdminService, AdminManager>();
 builder.Services.AddScoped<IAdminDal, EfAdminDal>();
 
 builder.Services.AddDbContext<AgricultureContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AgricultureContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddMvc(config =>
