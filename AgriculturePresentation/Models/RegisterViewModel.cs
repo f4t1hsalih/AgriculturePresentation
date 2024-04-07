c60d21b3ff33e1cf72daa31fab5f3188ce6e981e
f4t1hsalih <f4t1hsalih@gmail.com>
2024-04-07 14:28:33 +0300

    Ders 156 - SignUp Sayfası

---
+++ AgriculturePresentation/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriculturePresentation.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Bir Kullanıcı Adı Giriniz!")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Bir Mail Giriniz!")]
+        public string mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Giriniz!")]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Giriniz!")]
+        [Compare("password", ErrorMessage = "Şifreler Uyumlu değil, kontrol edin")]
+        public string passwordConfirm { get; set; }
+
+    }
+}
